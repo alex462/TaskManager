589fa9fb09f8b5bce310027eaadb7876c648be84
alex462 <alex.reinhart462@gmail.com>
2018-02-20 14:43:42 -0500

    all classes created and defined by header comments

--- src/com/company/List.java
+++ src/com/company/List.java
@@ -1,4 +1,7 @@
 package com.company;
 
+/*
+LIST: This class will hold all methods for both the Main Menu and the Edit Menu.
+ */
 public class List {
 }
--- src/com/company/Task.java
+++ src/com/company/Task.java
@@ -1,4 +1,7 @@
 package com.company;
 
+/*
+TASK: This class will define "task" and hold getters and setters for anything related to the definition of a "task"
+ */
 public class Task {
 }
