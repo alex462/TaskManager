578bf1c6569e3f2de93c0038f2506ee13e5139e4
alex462 <alex.reinhart462@gmail.com>
2018-02-20 15:36:36 -0500

    starting code in Main Menu class

--- src/com/company/MainMenu.java
+++ src/com/company/MainMenu.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Scanner;
+
 /*
 MAIN MENU: This class will output greeting to user, then basic/beginning functions of program:
 1. Create task
@@ -7,4 +9,28 @@ MAIN MENU: This class will output greeting to user, then basic/beginning functio
 3. Exit task manager
  */
 public class MainMenu {
+
+    private Scanner input = new Scanner(System.in);
+
+    protected void welcomePrompt(){
+
+        System.out.println("\nWELCOME TO THE TASK MANAGER");
+        startMenu();
+    }
+
+    protected void startMenu(){
+
+        System.out.println("\nWhat would you like to do?\n" +
+                "Enter the number corresponding to the action you choose:\n" +
+                "1. Create a new task\n" +
+                "2. View your tasks\n" +
+                "3. Exit task manager");
+
+        switch(input.nextLine()){
+
+            case "1": //create a new task
+                editMenu.createTask();
+
+        }
+    }
 }
