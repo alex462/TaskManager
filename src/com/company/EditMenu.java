52c47e2240a7817e9ba3bc1b00a125fdc150d5ed
alex462 <alex.reinhart462@gmail.com>
2018-02-20 14:16:49 -0500

    added all classes. adding comment headers describing individual class functions.

---
+++ src/com/company/EditMenu.java
@@ -0,0 +1,8 @@
+package com.company;
+
+/*
+EDIT MENU: This menu outputs editing options to user concerning their already existing tasks (which they have added from the Main Menu). They will also have different viewing options from this menu. The edit menu will initialize when option (2. view tasks) in the main menu is selected; the user's tasks will be listed then the following options will be listed:
+
+ */
+public class EditMenu {
+}
---
+++ src/com/company/MainMenu.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/*
+MAIN MENU: This class will output greeting to user, then basic/beginning functions of program:
+1. Create task
+2. View tasks
+3. Exit task manager
+ */
+public class MainMenu {
+}
